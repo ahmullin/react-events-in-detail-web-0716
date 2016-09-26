f0117e12a1393da6adf960ce0e29b91600e96450
Alison <alisonhmullin@gmail.com>
2016-09-25 21:51:55 -0400

    complete events in detail code along

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const app = "I don't do much.";
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+
+class Clicker extends React.Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    console.log(event.type); // prints 'click'
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>Click me!</button>
+    );
+  }
+}
